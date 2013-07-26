a33862837010feac34b0ac8a1428dc92272c8419
Anthony Short <antshort+github@gmail.com>
2013-07-26 22:40:24 +1000

    Added tests for other url types

--- index.js
+++ index.js
@@ -3,28 +3,7 @@ var crypto = require('crypto');
 var path = require('path');
 var Batch = require('batch');
 var mkdirp = require('mkdirp');
-
-function copy(from, to, callback) {
-  var write = fs.createWriteStream(to);
-  var read = fs.createReadStream(from);
-  write.on('close', callback);
-  write.on('error', callback);
-  read.on('error', callback);
-  read.pipe(write);
-}
-
-function isAbsolute(url) {
-  return ~url.indexOf('://') || url[0] == '/';
-};
-
-function isData(url) {
-  return 0 == url.indexOf('data:');
-};
-
-function stripQuotes(str) {
-  if ('"' == str[0] || "'" == str[0]) return str.slice(1, -1);
-  return str;
-};
+var utils = require('./lib/utils');
 
 function Bundler(options) {
   this.options = options;
@@ -44,15 +23,15 @@ Bundler.prototype.build = function(input, dest, callback) {
       var batch = new Batch();
 
       var css = str.replace(/\burl\s*\(([^)]+)\)/g, function(_, url) {
-        url = stripQuotes(url);
-        if( isAbsolute(url) || isData(url) ) return 'url(' + url + ')';
+        url = utils.stripQuotes(url);
+        if( utils.isAbsolute(url) || utils.isData(url) ) return 'url("' + url + '")';
         var filename = path.basename(url);
         var name = crypto.createHash('md5').update(url).digest("hex") + path.extname(filename);
         batch.push(function(done){
           var image = path.join(inputDir, url);
-          copy(image, path.join(dir, name), done);
+          utils.copy(image, path.join(dir, name), done);
         });
-        return 'url(' + name + ')';
+        return 'url("' + name + '")';
       });
 
       batch.push(function(done){
