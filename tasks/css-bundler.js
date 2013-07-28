6c11202f0db5ce2df34c7c35265b06d451b5760a
Anthony Short <antshort+github@gmail.com>
2013-07-28 21:29:46 +1000

    Added grunt task

---
+++ tasks/css-bundler.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+var Batch = require('batch');
+
+module.exports = function(grunt) {
+  grunt.registerMultiTask('cssbundler', function(){
+    var options = this.options({});
+    var batch = new Batch();
+    this.files.forEach(function(file){
+      file.src.forEach(function(filepath){
+        batch.push(function(done){
+          var bundle = new Bundle(options);
+          bundle.build(filepath, file.dest, function(err){
+            if(err) return grunt.log.error(err);
+            done();
+          });
+        });
+      });
+    });
+    batch.end(this.async());
+  });
+};
